c41157dd1fa3542e8831f9861e2de38405283078
Geovanni Souza <geovanni.dev23@hotmail.com>
2026-03-03 15:17:03 -0300

    feat: improve performance, clean debug, expand CI and ensure Go formatting

--- scripts/check_debug_prints.sh
+++ scripts/check_debug_prints.sh
@@ -4,7 +4,7 @@
 
 set -e
 
-if grep -rE 'fmt\.Print|log\.Print|println|print\(' --exclude-dir=.git --exclude-dir=vendor .; then
+if grep -rE 'fmt\.Print|log\.Print|println|print\(' --exclude-dir=.git --exclude-dir=vendor --exclude='scripts/check_debug_prints.sh' .; then
   echo "ERRO: Encontrado uso proibido de prints de debug!" >&2
   exit 1
 else
