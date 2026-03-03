c72bb2591d360004da0700f89924a6aa74dccd65
Geovanni Souza <geovanni.dev23@hotmail.com>
2026-03-03 15:30:24 -0300

    style(domain): format processor.go with gofmt

--- internal/domain/processor.go
+++ internal/domain/processor.go
@@ -84,8 +84,8 @@ func (p *Processor) AnalyzeFeed(req AnalyzeFeedRequest, now time.Time) (AnalyzeF
 		s := sentimentAnalyzer.Analyze(m)
 		sentiments = append(sentiments, s)
 		if !s.Excluded {
-			   distCount[s.Classification]++
-			   totalForDist++
+			distCount[s.Classification]++
+			totalForDist++
 		}
 	}
 	dist := FeedSentimentDistribution{Positive: 0, Neutral: 0, Negative: 0}
